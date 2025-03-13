b626f3e2a5f63477344b7375c806b730169d0f81
Noobulus <madeline.r.wilkerson@gmail.com>
2025-03-13 14:09:40 -0500

    Initial commit

---
+++ src/client/java/mod/noobulus/maddyorigins/MaddyOriginsClient.java
@@ -0,0 +1,10 @@
+package mod.noobulus.maddyorigins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaddyOriginsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
